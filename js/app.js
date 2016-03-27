d26526507e37a9cfba78e46b60eee1ea3ba8430b
Daniel Granger <dg1234uk@me.com>
2016-03-27 20:52:31 +0100

    Basic file structure - initial Commit

---
+++ js/app.js
@@ -0,0 +1,23 @@
+var asteroids = (function() {     // jshint ignore:line
+  var game = {
+    init: function() {
+
+    },
+
+    main: function() {
+
+    },
+
+    update: function() {
+
+    },
+
+    render: function() {
+
+    },
+
+  };
+
+  game.init();
+
+}());
